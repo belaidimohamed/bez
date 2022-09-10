2603211da944a3c7d671c14f9a825f50f17db94c
belaidimohamed <mohamedbelaidi408@gmail.com>
2022-09-10 15:01:28 +0100

    first commit

---
+++ tests/test.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+from database import *
+from bson import ObjectId
+import unittest
+from main import app
+
+client = TestClient(app)
+
+class ProfileInfo(unittest.TestCase):
+    def test_profile_saved(self):
+        response = client.post("/scrapPublicPage/",
+        json={"url":"https://www.facebook.com/barackobama/"}) 
+
+        assert response.status_code == 201
+        
